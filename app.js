39fd0161967e6e5fdcc671457decdfa076210a25
mirmammad.gafarov@gmail.com <mirmammad.gafarov@gmail.com>
2022-09-29 23:58:45 +0400

    Submit and post secret

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 })
 
 
@@ -155,12 +156,56 @@ app.get("/register",function(req,res){
 
 app.get("/secrets", function(req ,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        User.find({"secret": {$ne: null}}, function(err ,foundUsers){
+            if(err){
+                console.log(err)
+            }else{
+                if(foundUsers){
+                    res.render("secrets", {usersWithSecrets: foundUsers})
+                }
+            }
+        })
     } else {
         res.redirect("/login");
     }
+
+
+
+   
+
+
+    
 });
  
+
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+})
+
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                foundUser.secret = submittedSecret
+                foundUser.save(function(){
+                    res.redirect("/secrets")
+                })
+            }
+            
+
+        }
+    })
+})
+
 
 app.get("/logout", function(req, res){
     req.logout(function(err){
